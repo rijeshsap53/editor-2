7cc460fbf5818afa170d6616c83bb244f71e88dd
rijeshsap53 <110729636+rijeshsap53@users.noreply.github.com>
2022-10-15 21:17:30 +0545

    any messsage

--- script_.js
+++ script_.js
@@ -20,5 +20,37 @@ image_input.addEventListener("change",function(){
     });
     reader.readAsDataURL(this.files[0]);
 
-    console.log(image_input.value);
+    // console.log(image_input.value);
 });
+
+  
+ 
+// again upload img
+
+const option2 = document.querySelector(".aga_option");
+const dialoge2 = document.querySelector("#aga_dialouge");
+
+option2.addEventListener("click", function(){
+    dialoge2.click();
+    
+});
+
+const image_input2 = document.querySelector("#aga_dialouge");
+const inf2 = document.querySelector(".inf");
+let uploaded_image2 = "";
+
+image_input2.addEventListener("change",function(){
+    inf2.style.display="none";
+    
+    const reader = new FileReader();
+    reader.addEventListener("load", () => {
+    uploaded_image2 = reader.result;
+    document.querySelector(".upload_p").style.backgroundImage = 'url('+uploaded_image2+')';
+    });
+    reader.readAsDataURL(this.files[0]);
+
+    console.log(image_input2.value);
+});
+
+
+
